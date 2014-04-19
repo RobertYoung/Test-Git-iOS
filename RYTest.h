bd917808af552c3ba43e686197a454cc877fa54b
RobertYoung <noxious1991@googlemail.com>
2014-04-19 14:19:40 +0100

    added a test class

---
+++ RYTest.h
@@ -0,0 +1,13 @@
+//
+//  RYTest.h
+//  GitTest
+//
+//  Created by Robert Young on 19/04/2014.
+//  Copyright (c) 2014 Robert Young. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RYTest : NSObject
+
+@end
---
+++ RYTest.m
@@ -0,0 +1,13 @@
+//
+//  RYTest.m
+//  GitTest
+//
+//  Created by Robert Young on 19/04/2014.
+//  Copyright (c) 2014 Robert Young. All rights reserved.
+//
+
+#import "RYTest.h"
+
+@implementation RYTest
+
+@end
